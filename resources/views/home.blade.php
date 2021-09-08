5f3704d273ac0b6187e4dbd7dd62c8de9865c13a
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2021-09-08 21:22:20 +0200

    first commit in lesson

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 
-<p>Szia!</p>
+<div class="card">
+    <div class="card-content">
+      <span class="card-title">Sziasztok</span>
+      <p>Remélem szép ez a kártya.</p>
+    </div>
+</div>
 
 @endsection
