5d3da489926d7cf15f749bdc65238a21de9e75e4
Viktor Csimma <csimmaviktor03@gmail.com>
2021-09-09 10:25:12 +0200

    Added cards to the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,77 @@
 
 @section('content')
 
-<p>Szia!</p>
+<p>Welcome.</p>
+
+<div class="row">
+
+      <div class="col s12 m4 l3 red accent-1">
+        a
+      </div>
+      
+      <div class="col s12 m1 l2 indigo lighten-5">
+        b
+      </div> 
+
+      <div class="col s12 m7 l7 red accent-2">
+        c
+      </div>
+
+</div>
+
+<div class="row">
+    <div class="col s12 m6 l3">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card indigo lighten-2">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card red accent-4">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
