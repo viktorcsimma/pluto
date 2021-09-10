c9fb0f3dc628895e9aeadeadd3e912a7a485da73
Viktor Csimma <csimmaviktor03@gmail.com>
2021-09-10 18:32:54 +0200

    Added a nice terminal-like block

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,74 +5,48 @@
 <p>Welcome.</p>
 
 <div class="row">
-
-      <div class="col s12 m4 l3 red accent-1">
-        a
-      </div>
-      
-      <div class="col s12 m1 l2 indigo lighten-5">
-        b
-      </div> 
-
-      <div class="col s12 m7 l7 red accent-2">
-        c
-      </div>
-
+  <div class="blue-grey darken-3 white-text s12 z-depth-2 center-align">
+    <strong style="font-size: large">benutzer@white-laptop: ~</strong>
+  </div>
+  <div class="grey darken-4 white-text s12 z-depth-2 left-align" style="font-family: Monospace"><strong class="green-text">
+  benutzer@white-laptop:</strong><strong class="blue-text">~/minuteman</strong>$ minuteman-launch --now<br>
+  WARNING:<br>
+  The launch of intercontinental missiles without prior authorization from a superior officer is illegal and is punishable by military and international courts. It is critically important to use this program only on an official, written order. Make sure that such an order is stored at a safe place.<br><br>
+  
+  Are you sure you want to proceed? (Y/N) █
+  </div>
+  <div class="blue-grey darken-1 white-text s12 z-depth-2 center-align">
+  Maybe you are sure, but this is <strong>not</strong> a terminal window. Sorry about that.
+  </div>
+  
 </div>
 
-<div class="row">
-    <div class="col s12 m6 l3">
+
+  <div class="row">
+    <div class="col s12 m6 l6">
+      <div class="card red">
+        <div class="card-content white-text">
+          <span class="card-title">Not this time...</span>
+          <p>If you are interested in destroying the world through such and other means,
+          we have some ideas for you.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://en.wikipedia.org/wiki/Human_extinction">I'm in!</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l6">
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <span class="card-title">Come on...</span>
+          <p>Would rather make the world a better place?
+          (Actually I could promote anything here.)</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m6 l3">
-      <div class="card indigo lighten-2">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m6 l3">
-      <div class="card red accent-4">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m6 l3">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
+          <a href="http://example.com">Let's go!</a>
         </div>
       </div>
     </div>
   </div>
-
+           
 @endsection
